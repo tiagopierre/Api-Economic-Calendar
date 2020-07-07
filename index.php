5ab8980c940c992a1f945cb5252b9c98001773d4
Tiago Pierre de Mendonça <43191667+tiagopierre@users.noreply.github.com>
2020-07-07 12:33:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ header("Content-Type: application/json");
 include_once(getcwd() . "/vendor/kub-at/php-simple-html-dom-parser/src/KubAT/PhpSimple/HtmlDomParser.php");
 use KubAT\PhpSimple\HtmlDomParser;
 
-$dom = HtmlDomParser::file_get_html("https://sslecal2.forexprostools.com/", false, null, 0);
+$dom = HtmlDomParser::file_get_html("https://sslecal2.forexprostools.com/?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone&countries=110,17,29,25,32,6,37,36,26,5,22,39,14,48,10,35,7,43,38,4,12,72&calType=week&timeZone=12&lang=12", false, null, 0);
 $elems = $dom->getElementById("#ecEventsTable")->find("tr[id*='eventRowId']");
 $data = [];
 

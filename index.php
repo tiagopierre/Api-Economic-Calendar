efb80be6e81a869cd66cfa5dd7b2a57663d8e9ae
Tiago Pierre de Mendonça <43191667+tiagopierre@users.noreply.github.com>
2020-08-19 11:06:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Teste PHP</title>
  </head>
  <body>
- <iframe  src="https://tradingalertsapp.web.app" style="height:100%;width:100%" ></iframe>
+ <iframe  src="https://tradingalertsapp.web.app" height="100%" width="100%" ></iframe>
  </body>
 </html>

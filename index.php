8bd71a6757171cc641e0beba2feb2a58ff139291
Tiago Pierre de Mendonça <43191667+tiagopierre@users.noreply.github.com>
2020-08-21 23:19:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
   <title>Trading Alerts</title>
  </head>
- <body>
+ <body style="margin: 0px; padding: 0px;">
  <iframe  src="https://tradingalertsapp.web.app" style="height:100%;width:100%; border:0px; margin: 0px; padding: 0px" ></iframe>
  </body>
 </html>

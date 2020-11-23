9f7aa9be9e6316d3e144995242b8b65576d57fab
Tiago Pierre de Mendonça <43191667+tiagopierre@users.noreply.github.com>
2020-11-23 09:37:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <html>
  <head>
   <title>Trading Alerts</title>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8367599494759749",
+    enable_page_level_ads: true
+  });
+</script>
  </head>
  <body style="margin: 0px; padding: 0px;">
  <iframe  src="https://tradingalertsapp.web.app" style="height:100%;width:100%; border:0px; margin: 0px; padding: 0px" ></iframe>
